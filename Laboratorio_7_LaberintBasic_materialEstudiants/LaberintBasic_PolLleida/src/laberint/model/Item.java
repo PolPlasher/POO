6fb830a0525d373096ed3b3d3fea2bba556a6ed9
Pol Lleida <pol.lleida@estudiantat.upc.edu>
2023-04-24 13:26:02 +0200

    Item

--- Laboratorio_7_LaberintBasic_materialEstudiants/LaberintBasic_PolLleida/src/laberint/model/Item.java
+++ Laboratorio_7_LaberintBasic_materialEstudiants/LaberintBasic_PolLleida/src/laberint/model/Item.java
@@ -2,6 +2,33 @@ package laberint.model;
 
 /** Atributs i mètodes dels items. */
 public class Item {
+
+    private final String description;
+    private int encumbrance;
+    private final String name;
+    private int value;
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getEncumbrance() {
+        return this.encumbrance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEncumbrance(int enc) {
+        this.encumbrance = enc;
+    }
+
+    public setValue(int value) {
+        this.value = value;
+    }
+
     /** Retorna una String amb el valor dels atributs de l'Item.
      * @return la descripció textual de l'Item
      */
