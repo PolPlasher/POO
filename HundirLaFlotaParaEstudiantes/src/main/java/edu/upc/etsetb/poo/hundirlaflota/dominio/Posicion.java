704513bc8cf723225a7327d7de058b5fcfb849b5
Pol Lleida <pol.lleida@estudiantat.upc.edu>
2023-05-08 12:29:49 +0200

    ghf

--- HundirLaFlotaParaEstudiantes/src/main/java/edu/upc/etsetb/poo/hundirlaflota/dominio/Posicion.java
+++ HundirLaFlotaParaEstudiantes/src/main/java/edu/upc/etsetb/poo/hundirlaflota/dominio/Posicion.java
@@ -379,10 +379,8 @@ public class Posicion {
     public static Set<String> getAdyacentes(String posicion)
             throws PositionException {
         Set<String> lista = new HashSet<>();
-
         if (!Posicion.esCorrecta(posicion))
             throw new PositionException();
-
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 if (i != 0 || j != 0) {
